3965142ca80d65ab5e40a90528c65f536f6f16a1
Carlos Victor <54481508+carlosnet85@users.noreply.github.com>
2024-04-19 18:14:21 +0000

    Minor changes

--- hlb-project/src/Components/Footer/Footer.tsx
+++ hlb-project/src/Components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer: React.FC = () => {
     return ( 
         <S.FooterContainer>
             <S.FooterText>
-                <p>Website made by <a href="https://github.com/carlosnet85">This guy!</a></p>
+                <p>Website made by <a href="https://github.com/carlosnet85/hlb-fanmade-app">This guy!</a></p>
                 <p> Other than that, pretty much everything belongs to <a href="https://humanitys-last-breath.com/">Humanity's Last Breath. </a></p>
             </S.FooterText>
         </S.FooterContainer>
