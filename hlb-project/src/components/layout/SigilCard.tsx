cff1f5a08f2c195a6b2a8aae33e5c9b4fb083705
carlosnet85 <zlordofpowerz@gmail.com>
2025-02-17 16:55:22 -0300

    Layers now have random variants

--- hlb-project/src/components/layout/SigilCard.tsx
+++ hlb-project/src/components/layout/SigilCard.tsx
@@ -3,6 +3,8 @@ import * as S from "./SigilCard-style";
 import sigilLayer01 from "../../assets/sigils/sigilLayer01.png";
 import sigilLayer02 from "../../assets/sigils/sigilLayer02.png";
 import sigilLayer03 from "../../assets/sigils/sigilLayer03.png";
+import sigilLayer04 from "../../assets/sigils/sigilLayer04.png";
+import sigilLayer05 from "../../assets/sigils/sigilLayer05.png";
 
 import { Sigil } from "../../data/SigilsData";
 
@@ -17,6 +19,16 @@ const SigilCard: React.FC<SigilCardProps> = ({
   isSelected,
   onClick,
 }) => {
+  function getRandomLayers(layers: string[], count: number) {
+    const shuffled = [...layers].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, count);
+  }
+
+  const randomLayers = getRandomLayers(
+    [sigilLayer01, sigilLayer02, sigilLayer03, sigilLayer04, sigilLayer05],
+    3
+  );
+
   function handleClick() {
     onClick();
 
@@ -28,25 +40,16 @@ const SigilCard: React.FC<SigilCardProps> = ({
   return (
     <S.SigilCardContainer isSelected={isSelected} onClick={handleClick}>
       <S.SigilContainer>
-        <S.SigilLayer
-          src={sigilLayer01}
-          alt="Sigil Logo"
-          $size="100%"
-          $rotateSpeed="65s"
-        />
-        <S.SigilLayer
-          src={sigilLayer02}
-          alt="Sigil Logo"
-          $size="90%"
-          $rotateSpeed="50s"
-          $reverseRotation
-        />
-        <S.SigilLayer
-          src={sigilLayer03}
-          alt="Sigil Logo"
-          $size="80%"
-          $rotateSpeed="45s"
-        />
+        {randomLayers.map((layer) => (
+          <S.SigilLayer
+            key={layer}
+            src={layer}
+            alt="Sigil Logo"
+            $size={`${100 - randomLayers.indexOf(layer) * 10}%`}
+            $rotateSpeed={`${65 - randomLayers.indexOf(layer) * 15}s`}
+            $reverseRotation={randomLayers.indexOf(layer) % 2 === 0}
+          />
+        ))}
         <S.SigilLayer
           src={sigil.image}
           alt="Sigil Logo"
