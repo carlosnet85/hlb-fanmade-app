4fb5239de084474787b9fe52d3063c9e6ad13a28
carlosnet85 <zlordofpowerz@gmail.com>
2026-01-03 17:06:30 -0300

    uh

--- hlb-project/vite.config.ts
+++ hlb-project/vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate', 
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       workbox: {
         globPatterns: ['**/*.{js,css,html,png,svg,jpg,jpeg,webp}'],
         maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, 
